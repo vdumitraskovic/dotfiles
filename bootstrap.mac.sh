4a0eb88989d21137580563a4a73116ab1240ebec
Vladimir Dumitraskovic <user@vdumitraskovic.com>
2023-07-07 17:01:21 +0200

    Add apps config

--- bootstrap.mac.sh
+++ bootstrap.mac.sh
@@ -107,3 +107,17 @@ brew install meld
 
 # Setup source-highlight
 brew install source-highlight
+
+# Install some apps
+brew install visual-studio-code
+brew install firefox
+brew install google-chrome
+brew install microsoft-edge
+brew install slack
+brew install notion
+
+# Install apps from marketplace
+brew install mas
+mas install 1451685025 # WireGuard
+mas install 585829637  # Todoist
+mas install 732710998  # Enpass
