4d22b4d4f8101387c27ed0fe5210b1c05310b429
Vladimir Dumitraskovic <vladimir@wonderdynamics.com>
2024-08-26 16:39:34 +0200

    Add telescope fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,20 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	init = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			pickers = {
+				git_files = {
+					show_untracked = true,
+				},
+			},
+		})
+		telescope.load_extension("fzf")
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "", builtin.git_files, { silent = true, desc = "Open Files" })
 	end,
