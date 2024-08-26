ab6f716c473af70f3f3ee5aa57ebcf539ae542bd
Vladimir Dumitraskovic <vladimir@wonderdynamics.com>
2024-08-26 16:43:00 +0200

    Add some keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 
-require 'config.lazy'
+require("config.lazy")
+require("config.keymaps")
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader><space>", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>")
