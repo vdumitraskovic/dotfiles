3367355c9ca1f04f3bfc1b2d2f458b5e326b1b5e
Vladimir Dumitraskovic <vladimir@wonderdynamics.com>
2024-08-26 16:42:09 +0200

    Add devicons

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	init = function()
 		local telescope = require("telescope")
