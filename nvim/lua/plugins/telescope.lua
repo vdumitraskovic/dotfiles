4630b99ee4afa4bbbc7610bcbf8047f41a1e85ee
Vladimir Dumitraskovic <vladimir@wonderdynamics.com>
2024-08-29 13:38:09 +0200

    Set ui select to telescope
    Make selections more visually pleasing in Neovim.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,18 +5,28 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
 		local telescope = require("telescope")
+		local themes = require("telescope.themes")
+
 		telescope.setup({
 			pickers = {
 				git_files = {
 					show_untracked = true,
+					theme = "dropdown",
+				},
+			},
+			extensions = {
+				["ui-select"] = {
+					themes.get_cursor(),
 				},
 			},
 		})
 		telescope.load_extension("fzf")
+		telescope.load_extension("ui-select")
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "", builtin.git_files, { silent = true, desc = "Open Files" })
 	end,
