7ce6bcc7df4f9f2d7877eea92e61634ec366103e
Vladimir Dumitraskovic <vladimir@wonderdynamics.com>
2024-10-08 10:57:37 +0200

    Set undofile
    It is really nice when you can preserve your changes over time.
    Very useful when changing contexts and closing the editor.

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 vim.o.smartcase = true
 vim.o.ignorecase = true
 vim.o.grepprg = "rg --smart-case --vimgrep"
+vim.o.undofile = true
 
 require("config.lazy")
 require("config.keymaps")
