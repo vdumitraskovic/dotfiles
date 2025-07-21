8c40304817fa1ac6e19836ec49abb38e17435ef4
Vladimir Dumitraskovic <vladimir@wonderdynamics.com>
2025-07-21 15:46:53 +0200

    Prevent double lines in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -38,5 +38,17 @@ return {
 		})
 		telescope.load_extension("fzf")
 		telescope.load_extension("ui-select")
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "TelescopeFindPre",
+			callback = function()
+				vim.opt_local.winborder = "none"
+				vim.api.nvim_create_autocmd("WinLeave", {
+					once = true,
+					callback = function()
+						vim.opt_local.winborder = "rounded"
+					end,
+				})
+			end,
+		})
 	end,
 }
