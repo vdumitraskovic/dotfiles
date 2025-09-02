a8c1c89ffeeadae279db27ee1c0a37d271340220
Vladimir Dumitraskovic <vladimir@wonderdynamics.com>
2025-09-02 10:12:47 +0200

    Configure tab stops

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,11 @@ vim.o.grepprg = "rg --smart-case --vimgrep"
 vim.o.undofile = true
 vim.o.foldlevel = 5
 vim.o.winborder = "rounded"
+vim.o.breakindent = true
+vim.o.smartindent = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.shiftround = true
 
 require("config.lazy")
 require("config.keymaps")
