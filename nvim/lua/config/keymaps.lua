a6745bcfc14d9b9263288fff83d63cafcb1a67b1
Vladimir Dumitraskovic <user@vdumitraskovic.com>
2025-10-11 18:21:48 +0200

    Replace telescope with snacks

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,13 @@ vim.keymap.set("n", "<leader><space>", "<cmd>nohlsearch<cr>")
 
 vim.keymap.set("n", "<leader>w", "<cmd>:w<cr>", { desc = "[W] Write" })
 
-vim.keymap.set("n", "", require("telescope.builtin").git_files, { desc = "Open Files" })
-vim.keymap.set("n", "<leader><enter>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-vim.keymap.set("n", "<leader>ld", require("telescope.builtin").diagnostics, { desc = "Find [L]SP [d]iagnostics" })
+vim.keymap.set("n", "", function()
+	require("snacks").picker.smart()
+end, { desc = "Smart Find Files" })
+vim.keymap.set("n", "<leader><enter>", function()
+	require("snacks").picker.buffers()
+end, { desc = "[ ] Find existing buffers" })
+
+vim.keymap.set("n", "<leader>ld", function()
+	require("snacks").picker.diagnostics_buffer()
+end, { desc = "Find [L]SP [d]iagnostics" })
